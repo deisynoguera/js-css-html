eb34f166cc695406f6e3aa6c01def0736ad2718b
VicenteFleitas <vicente1fleitas1@gmail.com>
2025-02-23 20:58:43 -0300

    creamos la clase stage

---
+++ game/biblioteca/Stage.js
@@ -0,0 +1,18 @@
+class Stage {
+  constructor(x, y, width, height, color) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.color = color;
+    // crear canvas
+    this.canvas = document.createElement("canvas");
+    this.canvas.width = this.width;
+    this.canvas.height = this.height;
+    this.canvas.style.background = this.color;
+    document.body.appendChild(this.canvas);
+    document.body.style.margin = 0;
+  }
+}
+
+export default Stage;
---
+++ game/main.js
@@ -0,0 +1,3 @@
+import Stage from "./biblioteca/Stage.js";
+
+let stage = new Stage(0, 0, 512, 256, "#cdcdcd");
